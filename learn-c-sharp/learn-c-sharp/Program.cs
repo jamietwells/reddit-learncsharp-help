cc678a52368f41d08fb5b940903c7f9ddd86fb58
Jamie Twells <jtwells3@googlemail.com>
2018-02-18 18:14:16 +0000

    Extracted some methods.

--- learn-c-sharp/learn-c-sharp/Program.cs
+++ learn-c-sharp/learn-c-sharp/Program.cs
@@ -5,16 +5,22 @@ namespace LearnCSharp {
     class Program {
 
         static void Main(string[] args) {
-            Console.WriteLine("=====================================");
-            Console.WriteLine("Prime Factorisation");
-            Console.WriteLine("=====================================");
-            
+            PrintIntroduction();
+
             Console.WriteLine("Please enter a MIN value:");
             int MIN = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter a MAX value:");
             int MAX = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadKey();
+            Pause();
+        }
+
+        private static void Pause() => Console.ReadKey();
+
+        private static void PrintIntroduction() {
+            Console.WriteLine("=====================================");
+            Console.WriteLine("Prime Factorisation");
+            Console.WriteLine("=====================================");
         }
     }
 }
