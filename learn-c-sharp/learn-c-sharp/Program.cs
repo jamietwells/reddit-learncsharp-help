d65b33f2638475c546183ae7d1a7e7eb62de437a
Andrew Erickson <gc2629yv@my.nhcc.edu>
2018-02-18 12:09:31 -0600

    console.readline for min and max inputs

--- learn-c-sharp/learn-c-sharp/Program.cs
+++ learn-c-sharp/learn-c-sharp/Program.cs
@@ -8,6 +8,12 @@ namespace LearnCSharp {
             Console.WriteLine("=====================================");
             Console.WriteLine("Prime Factorisation");
             Console.WriteLine("=====================================");
+            
+            Console.WriteLine("Please enter a MIN value:");
+            int MIN = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a MAX value:");
+            int MAX = Convert.ToInt32(Console.ReadLine());
+
             Console.ReadKey();
         }
     }
